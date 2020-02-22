aeb4d860b89c4c0fbfac4a448fd0a57df2e5101f
daruiz31 <d4ruiz31@gmail.com>
2020-02-21 22:44:45 -0500

    link to Counter.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -79,6 +79,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The corresponding controller for this page is found at:</p>
 		<code>application/controllers/Welcome.php</code>
 
+		<p>Counter:</p>
+		<code>application/controllers/Counter.php</code>
+
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
