dc3d8554e991748113651e5c199a41b9212d19d6
daruiz31 <d4ruiz31@gmail.com>
2020-02-23 14:37:29 -0500

    Change access config

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '104.197.97.255',
+	'username' => 'admin',
+	'password' => 'diego',
+	'database' => 'test',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
