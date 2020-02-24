7ad05bc145a639b545d8730c3457e574fce523b7
daruiz31 <d4ruiz31@gmail.com>
2020-02-23 20:10:35 -0500

    vue url fixed

--- application/views/counter.php
+++ application/views/counter.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Integrando VUE.JS Y AXIOS</title>
-	<meta charset="UTF-8">		
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.18/vue.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>	
+  <title>My first Vue app</title>
+  <script src="https://unpkg.com/vue"></script>
 </head>
 <body>
+  <div id="app">
+    {{ message }}
+  </div>
 
-	<div id="app">
- 		<p>Hola {{mensaje}}</p>
-	</div>
-
-	<script>
-		var app=new vue({
-			el:"#app",
-			data:{
-				mensaje:" Bienvenido "
-			}
-		})
-</script>
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Hello Vue!'
+      }
+    })
+  </script>
 </body>
 </html>
